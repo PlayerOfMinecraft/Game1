42cae80ec4b63ca4f628e0dd8e647c218525d22c
PlayerOfMinecraft <96972638+PlayerOfMinecraft@users.noreply.github.com>
2022-02-23 09:56:15 +0000

    added a 4th and final hazard

--- main.ts
+++ main.ts
@@ -10,12 +10,22 @@ let Lives = 3
 let sprite = game.createSprite(2, 4)
 let Hazard = game.createSprite(randint(0, 4), 0)
 let Hazard_2 = game.createSprite(randint(0, 4), 0)
+let Hazard_3 = game.createSprite(randint(0, 4), 0)
+let Hazard_4 = game.createSprite(randint(0, 4), 0)
 basic.forever(function () {
     Hazard_2.change(LedSpriteProperty.Y, 1)
     basic.pause(Difficulty)
 })
 basic.forever(function () {
     Hazard.change(LedSpriteProperty.Y, 1)
+    basic.pause(Difficulty)
+})
+basic.forever(function () {
+    Hazard_3.change(LedSpriteProperty.Y, 1)
+    basic.pause(Difficulty)
+})
+basic.forever(function () {
+    Hazard_4.change(LedSpriteProperty.Y, 1)
     basic.pause(Difficulty)
 })
 basic.forever(function () {
@@ -33,8 +43,6 @@ basic.forever(function () {
         if (Hazard.get(LedSpriteProperty.Y) == 4) {
             Hazard.delete()
             Hazard = game.createSprite(randint(0, 4), 0)
-            game.addScore(1)
-            Difficulty += -50
         }
     }
 })
@@ -45,6 +53,26 @@ basic.forever(function () {
         if (Hazard_2.get(LedSpriteProperty.Y) == 4) {
             Hazard_2.delete()
             Hazard_2 = game.createSprite(randint(0, 4), 0)
+        }
+    }
+})
+basic.forever(function () {
+    if (sprite.isTouching(Hazard_4)) {
+        Lives += -1
+    } else {
+        if (Hazard_4.get(LedSpriteProperty.Y) == 4) {
+            Hazard_4.delete()
+            Hazard_4 = game.createSprite(randint(0, 4), 0)
+        }
+    }
+})
+basic.forever(function () {
+    if (sprite.isTouching(Hazard_3)) {
+        Lives += -1
+    } else {
+        if (Hazard_3.get(LedSpriteProperty.Y) == 4) {
+            Hazard_3.delete()
+            Hazard_3 = game.createSprite(randint(0, 4), 0)
             game.addScore(1)
             Difficulty += -50
         }
